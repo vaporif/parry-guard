fabc58aaf22f2b0e1ecb5c4afffc3a2cbda97119
Dmytro Onypko <vaporif@proton.me>
2026-03-19 20:19:55 +0000

    flake update

--- flake.nix
+++ flake.nix
@@ -228,7 +228,7 @@
             RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
           }
           // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl];
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl pkgs.stdenv.cc.cc.lib];
           }
           // pkgs.lib.optionalAttrs onnxSupported {
             ORT_DYLIB_PATH = onnxArgs.ORT_DYLIB_PATH;
